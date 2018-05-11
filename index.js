ac927bd465b3f5ff067c0aa717feb8cafee58160
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-11 22:00:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ class President{
     this.yearsInOffice = yearsInOffice
     this.homeState = homeState
   }
+
+  veto(){
+    return 'NO!'
+  }
+
+  passBill(){
+    return "You can do that!"
+  }
+
+  doCharity(){
+    return "I like to help people."
+  }
+
+  conductPressInterview(){
+    return "I am proud to be American."
+  }
+
+  sayHi(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}
+    I represent the ${politicalParty}s, and was in office ${this.yearsInOffice}.`
+  }
 }
